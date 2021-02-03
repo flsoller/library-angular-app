e8c9d6a4e8e1a85272a18b5eaa16948d31b9a195
Florian Soller <flsoller@gmail.com>
2021-02-03 20:47:59 +0100

    Create initial folder and component structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './toolbars/title-bar/title-bar.component';
+import { MenuBarComponent } from './toolbars/menu-bar/menu-bar.component';
+import { BookCardComponent } from './books/book-card/book-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent,
+    MenuBarComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule
