953797eb47cb4935233a8d8159673b049916c386
Florian Soller <flsoller@gmail.com>
2021-02-07 11:19:42 +0100

    Add sample data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Book } from './books/book.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  books: Book[] = [
+    new Book('Some Title 1', 'Some Author 1', 100, true, false, false),
+    new Book('Some Title 2', 'Some Author 2', 200, false, true, false),
+    new Book('Some Title 3', 'Some Author 3', 300, false, false, true),
+  ];
+}
