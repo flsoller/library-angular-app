387082c8480318b546f130119ff3880afd7c207b
Florian Soller <flsoller@gmail.com>
2021-02-11 16:28:35 +0100

    Generate book modal component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TitleBarComponent } from './toolbars/title-bar/title-bar.component';
 import { MenuBarComponent } from './toolbars/menu-bar/menu-bar.component';
 import { BookCardComponent } from './books/book-card/book-card.component';
+import { BookModalComponent } from './books/book-modal/book-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleBarComponent,
     MenuBarComponent,
-    BookCardComponent
+    BookCardComponent,
+    BookModalComponent
   ],
   imports: [
     BrowserModule
