e5b072b283fda639633b4d4699113eeb55fc61b8
Florian Soller <flsoller@gmail.com>
2021-02-12 22:26:10 +0100

    Add method for changing modal display status.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Book } from './books/book.model';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showModal = false;
+
   books: Book[] = [
     new Book('Some Title 1', 'Some Author 1', 100, true, false, false),
     new Book('Some Title 2', 'Some Author 2', 200, false, true, false),
     new Book('Some Title 3', 'Some Author 3', 300, false, false, true),
   ];
+
+  displayModal(status: boolean) {
+    this.showModal = status;
+  }
 }
