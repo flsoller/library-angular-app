4144062a2dca294ae7146cac1897e6cd27a8af78
Florian Soller <flsoller@gmail.com>
2021-02-19 17:34:54 +0100

    Add FormsModule to handle data binding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './toolbars/title-bar/title-bar.component';
@@ -13,12 +14,10 @@ import { BookModalComponent } from './books/book-modal/book-modal.component';
     TitleBarComponent,
     MenuBarComponent,
     BookCardComponent,
-    BookModalComponent
+    BookModalComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
