d905d0662c965b6cf971c26f4b96b74ab3234499
Florian Soller <flsoller@gmail.com>
2021-02-21 17:03:10 +0100

    Add functions for reading input values. Add event emitter for new book instance.

--- src/app/books/book-modal/book-modal.component.ts
+++ src/app/books/book-modal/book-modal.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-book-modal',
   templateUrl: './book-modal.component.html',
-  styleUrls: ['./book-modal.component.css']
+  styleUrls: ['./book-modal.component.css'],
 })
-export class BookModalComponent implements OnInit {
+export class BookModalComponent {
+  @ViewChild('title') titleInputRef: ElementRef;
+  @ViewChild('author') authorInputRef: ElementRef;
+  @ViewChild('pages') pagesInputRef: ElementRef;
+  @ViewChild('isFav') isFavInputRef: ElementRef;
+  @ViewChild('isReading') isReadingInputRef: ElementRef;
+  @ViewChild('isLoaned') isLoanedInputRef: ElementRef;
 
-  constructor() { }
+  @Output() addBook = new EventEmitter<Book>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  onAddBook() {
+    const titleInput = this.titleInputRef.nativeElement.value;
+    const authorInput = this.authorInputRef.nativeElement.value;
+    const pagesInput = this.pagesInputRef.nativeElement.value;
+    const isFavInput = this.isFavInputRef.nativeElement.value;
+    const isReadingInput = this.isReadingInputRef.nativeElement.value;
+    const isLoanedInput = this.isLoanedInputRef.nativeElement.value;
+
+    const newBook = new Book(
+      titleInput,
+      authorInput,
+      pagesInput,
+      isFavInput,
+      isLoanedInput,
+      isReadingInput
+    );
+    this.addBook.emit(newBook);
   }
-
 }
