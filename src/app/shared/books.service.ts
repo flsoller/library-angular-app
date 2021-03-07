876034f7ddfde10c2426d01db8eb89bfc8711445
Florian Soller <flsoller@gmail.com>
2021-03-07 11:55:37 +0100

    Add method to get books of status: isReading

--- src/app/shared/books.service.ts
+++ src/app/shared/books.service.ts
@@ -1,15 +1,20 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Book } from '../books/book.model';
+import { FilterService } from './filter.service';
 
 @Injectable({ providedIn: 'root' })
 export class BooksService {
   libraryChanged = new EventEmitter<Book[]>();
+
+  constructor(private filterService: FilterService) {}
 
   private books: Book[] = [
     new Book('Some Title 1', 'Some Author 1', 100, true, false, false),
     new Book('Some Title 2', 'Some Author 2', 200, false, true, false),
     new Book('Some Title 3', 'Some Author 3', 300, false, false, true),
   ];
+
+  private filteredBooks: Book[] = [];
 
   getBooks() {
     return [...this.books];
@@ -25,5 +30,10 @@ export class BooksService {
       return book.title === title ? null : book;
     });
     this.libraryChanged.emit([...this.books]);
+  }
+
+  getIsReading() {
+    this.filteredBooks = this.filterService.viewIsReading(this.books);
+    this.libraryChanged.emit([...this.filteredBooks]);
   }
 }
