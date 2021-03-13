df9d40b472dbb50575a96221e1ff7d1d9bddd401
Florian Soller <flsoller@gmail.com>
2021-03-13 17:23:20 +0100

    Create service to handle local storage.

---
+++ src/app/shared/storage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Book } from '../books/book.model';
+
+@Injectable({ providedIn: 'root' })
+export class StorageService {
+  STORAGE_STRING = 'angular-library';
+
+  getFromLocalStorage(): Book[] {
+    try {
+      return localStorage.getItem(this.STORAGE_STRING)
+        ? JSON.parse(localStorage.getItem(this.STORAGE_STRING))
+        : [];
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  saveToLocalStorage(library: Book[]) {
+    try {
+      const serializedLibrary = JSON.stringify(library);
+      localStorage.setItem(this.STORAGE_STRING, serializedLibrary);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
