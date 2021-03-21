87c91270259de3aabb140cbfd587f4f5e2a4fa06
Florian Soller <flsoller@gmail.com>
2021-03-21 12:03:16 +0100

    Create dropdown for sort buttons (mobile friendly)

--- src/app/toolbars/menu-bar/menu-bar.component.ts
+++ src/app/toolbars/menu-bar/menu-bar.component.ts
@@ -8,10 +8,16 @@ import { ModalService } from 'src/app/shared/modal.service';
   styleUrls: ['./menu-bar.component.css'],
 })
 export class MenuBarComponent {
+  menuSort = false;
+
   constructor(
     private modalService: ModalService,
     private booksService: BooksService
   ) {}
+
+  onMenuSortToggle() {
+    this.menuSort = !this.menuSort;
+  }
 
   onModalOpen() {
     this.modalService.toggleModal();
@@ -19,17 +25,21 @@ export class MenuBarComponent {
 
   onGetAll() {
     this.booksService.getAll();
+    this.onMenuSortToggle();
   }
 
   onGetIsReading() {
     this.booksService.getIsReading();
+    this.onMenuSortToggle();
   }
 
   onGetIsFav() {
     this.booksService.getIsFav();
+    this.onMenuSortToggle();
   }
 
   onGetIsLoaned() {
     this.booksService.getIsLoaned();
+    this.onMenuSortToggle();
   }
 }
