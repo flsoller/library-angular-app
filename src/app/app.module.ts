c127f1845ec36b0df11c18dc580d371a4c35f615
Florian Soller <flsoller@gmail.com>
2021-04-14 22:25:22 +0200

    Import slider module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './toolbars/title-bar/title-bar.component';
@@ -16,7 +17,12 @@ import { BookModalComponent } from './books/book-modal/book-modal.component';
     BookCardComponent,
     BookModalComponent,
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
