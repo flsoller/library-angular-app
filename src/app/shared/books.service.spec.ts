ddb5fdff40b2bdf1b48419c535a7fe076d821bd8
Florian Soller <flsoller@gmail.com>
2021-04-19 17:20:56 +0200

    Add test for updating books.
    Set service private property to mockBookLibrary through bracket notation

--- src/app/shared/books.service.spec.ts
+++ src/app/shared/books.service.spec.ts
@@ -3,6 +3,10 @@ import { Book } from '../books/book.model';
 import { FilterService } from './filter.service';
 import { StorageService } from './storage.service';
 
+const mockBookLibrary = [
+  new Book(' Title', 'Some Author', 100, false, false, false),
+];
+
 describe('BooksService', () => {
   let service: BooksService;
   let filter: FilterService;
@@ -14,17 +18,20 @@ describe('BooksService', () => {
     storage = new StorageService();
     service = new BooksService(filter, storage);
     emitterSpy = jest.spyOn(service.libraryChanged, 'emit');
+
+    service['books'] = mockBookLibrary;
   });
 
   afterEach(() => {
     emitterSpy.mockRestore();
+    jest.clearAllMocks();
   });
 
   it('Adds a new book to library', () => {
     let book: Book = new Book(
-      'Some Title 4',
-      'Some Author 4',
-      400,
+      'New Book Title',
+      'New Book Author',
+      200,
       false,
       false,
       false
@@ -34,8 +41,8 @@ describe('BooksService', () => {
     service.addBook(book);
 
     expect(storageSpy).toHaveBeenCalled();
-    expect(service.getBooks().length).toBe(1);
-    expect(service.getBooks()[0].author).toBe('Some Author 4');
+    expect(service.getBooks().length).toBe(2);
+    expect(service.getBooks()[1].author).toBe('New Book Author');
     expect(emitterSpy).toHaveBeenCalled();
 
     storageSpy.mockRestore();
@@ -47,32 +54,32 @@ describe('BooksService', () => {
     service.getBooks();
 
     expect(storageSpy).toHaveBeenCalled();
-    expect(service.getBooks().length).toBe(1);
+    expect(service.getBooks().length).toBe(2);
 
     storageSpy.mockRestore();
+  });
+
+  it('Updates book', () => {
+    const storageSpy = jest.spyOn(storage, 'saveToLocalStorage');
+
+    service.updateBook('New Book Title', true, true, true);
+
+    expect(storageSpy).toBeCalled();
+    expect(service.getBooks()[1].isFav).toBe(true);
+    expect(service.getBooks()[1].isLoaned).toBe(true);
+    expect(service.getBooks()[1].isReading).toBe(true);
   });
 
   it('Removes book from library', () => {
     const storageSpy = jest.spyOn(storage, 'saveToLocalStorage');
 
-    let book: Book = new Book(
-      'Some Title 4',
-      'Some Author 4',
-      400,
-      false,
-      false,
-      false
-    );
-
-    service.addBook(book);
-
     // Does not remove any books
     service.removeBook('Does not exist');
-    expect(service.getBooks().length).toBe(1);
+    expect(service.getBooks().length).toBe(2);
 
     // Does remove book
-    service.removeBook('Some Title 4');
-    expect(service.getBooks().length).toBe(0);
+    service.removeBook('New Book Title');
+    expect(service.getBooks().length).toBe(1);
 
     expect(storageSpy).toHaveBeenCalled();
     expect(emitterSpy).toHaveBeenCalled();
